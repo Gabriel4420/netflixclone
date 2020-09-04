76f9862356ae1629ac9fe0777acbae7a942490de
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2020-09-04 03:04:08 -0300

    updates in files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Tmdb from './Tmdb';
 import './App.css';
-
+import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeatureMovie';
+import Header from './components/Header';
 function App() {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () =>{
+      // pegando a lista totally
+
+      let list = await Tmdb.getHomeList();
+
+      setMovieList(list);
+
+      // pegando o filme em destaque (featured)
+      let originals = list.filter(i => i.slug ==='originals');
+      let randomChosen = Math.floor(Math.random()*(originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id,'tv');
+      
+      setFeatureData(chosenInfo);
+    }
+    loadAll()
+  }, [])
+
+  useEffect(() =>{
+    const scrollListener = () => {
+      if(window.scrollY > 10){
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll',scrollListener)
+    }
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="page">
+        
+        <Header black={blackHeader} />
+
+        {featuredData &&
+          <FeatureMovie item={featuredData}/>
+        }
+
+        <section className="lists">
+          {movieList.map((item,key) =>(
+            <MovieRow key={key} title={item.title} items={item.items}/>
+            ))}
+        </section>
+
+        <footer>
+          Development by Gabriel Rodrigues <br/>
+          all rights reserved &copy; Netflix <br/>
+          api database: tmdb.com
+        </footer>
+      </div>
   );
+  
 }
 
 export default App;
---
+++ src/components/FeatureMovie/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './style.css';
+
+export default ({item}) => {
+  let firstDate = new Date(item.first_air_date);
+
+  let genres = [];
+
+  for(let i in item.genres){
+    genres.push(item.genres[i].name);
+  }
+  
+  let titulo = item.original_name;
+
+  if(titulo == 'Law & Order: Special Victims Unit'){
+    titulo = titulo.substring(0,11);
+  }
+  
+  let description = item.overview;
+
+  if(description.length > 200){
+    description = description.substring(0,200) + ' ...';
+  }
+
+  return ( 
+    <section className="featured" style={{
+      backgroundSize:'cover',
+      backgroundPosition: 'center',
+      backgroundImage:`url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+    }}>
+      <div className="featured--vertical">
+        <div className="featured--horizontal">
+          <div className="featured--name">{titulo}</div>
+            <div className="featured--info">
+                <div className="featured--points">{item.vote_average} pts</div>
+                <div className="featured--year">{firstDate.getFullYear()}</div>
+                <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's':''}</div>
+            </div>
+            <div className="featured--description">{description}</div>
+            <div className="featured--buttons">
+                
+                <a key="0" href={`/watch/${item.id}`} className="featured--wbutton"> ► Assistir</a>
+                <a href={`/list/add/${item.id}`} className="featured--mylistButton">+ Minha Lista</a>
+            </div>
+            <div className="featured--genres">
+              <span><strong>Gêneros:</strong> {genres.join(' , ')}</span>
+            </div>
+        </div>
+
+      </div>
+    </section>
+  )
+}
